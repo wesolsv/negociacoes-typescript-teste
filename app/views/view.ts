1bcf0558fa6d7a866b977fe030a196f152cd784b
Wesley Oliveira Silva <wesolsv@gmail.com>
2023-03-23 17:56:34 -0300

    trabalhando com strict null check e casting

--- app/views/view.ts
+++ app/views/view.ts
@@ -5,7 +5,12 @@ export abstract class View<T>{
     private escapar = false;
 
     constructor(seletor:string, escapar?: boolean){
-        this.elemento = document.querySelector(seletor);
+        const elemento = document.querySelector(seletor);
+        if(elemento){
+            this.elemento = elemento as HTMLElement;
+        }else{
+            throw Error(`Seletor ${seletor} não existe no DOM!`)
+        }
         if(escapar) this.escapar = escapar;
     }
 
--- dist/js/app.js
+++ dist/js/app.js
@@ -1,4 +1,9 @@
 import { NegociacaoController } from "./controllers/negociacao-controller.js";
 const controller = new NegociacaoController();
 const form = document.querySelector(".form");
-form.addEventListener('submit', event => { event.preventDefault(); controller.adiciona(); });
+if (form) {
+    form.addEventListener('submit', event => { event.preventDefault(); controller.adiciona(); });
+}
+else {
+    throw Error('Não foi possível inicializar a aplicação, verifique se o form existe!');
+}
